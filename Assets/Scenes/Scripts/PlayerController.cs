b819d6941b21681d751379a72338a1c1a1614a2d
RuteGomesTM <164046128+RuteGomesTM@users.noreply.github.com>
2024-05-20 11:29:31 +0100

    Lição 1.4 - Assuma o assento do motorista

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,10 +13,18 @@ public class NewBehaviourScript : MonoBehaviour
 
     // Update is called once per frame
 
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnspeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // Mover o veículo para a frente
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rodar o veículo para os lados
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+
     }
 }
